75092cf10c39416721d801ab87b1936b1d250028
shintojoseph1234 <shintojoseph1234@gmail.com>
2017-03-07 20:18:28 +0530

    Update views.py

--- assetallocation/views.py
+++ assetallocation/views.py
@@ -11,16 +11,13 @@ def index(request):
 
     # variables to give inputs
 
-    # start_year = input("enter start year")
-    # end_year = input("enter end year")
-    # input_price = input("enter the input price")
-    # time_period = raw_input(" W for weekly, M for monthly, Q for quarterly, A for yearly")
+    start_year = input("enter start year")
+    end_year = input("enter end year")
+    input_price = input("enter the input price")
+    time_period = raw_input(" W for weekly, M for monthly, Q for quarterly, A for yearly")
     number_of_stocks = int(input("enter the number of stocks"))
 
-    start_year = 2007
-    end_year = 2008
-    input_price = 500000
-    time_period = "Q"
+   
 
     stock_symbols = stocks(number_of_stocks)
 
